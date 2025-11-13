8b78d33a15f1b719fa71e9f28037819a84a89843
valeria christina <valeriachristina93@gmail.com>
2025-11-13 04:45:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6271486974-90490",
+  "appId": "1:409579772224:web:d7adb9ef2667de287e82a5",
+  "apiKey": "AIzaSyARYzBj4JhPl1niTiCfMJF_P7rfSjyeFiw",
+  "authDomain": "studio-6271486974-90490.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "409579772224"
+};
